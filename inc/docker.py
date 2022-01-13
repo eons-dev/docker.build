1ba9b488fc8789adfac2004247cc033a4dbad5e7
eons <eons@eons.dev>
2022-01-13 11:18:27 -0800

    Can build docker containers for building docker containers, so it should work!

--- inc/docker.py
+++ inc/docker.py
@@ -16,61 +16,64 @@ class docker(Builder):
         self.optionalKWArgs["base_image"] = None
         self.optionalKWArgs["install"] = []
         self.optionalKWArgs["image_os"] = None
+        self.optionalKWArgs["image_name"] = None
+        self.optionalKWArgs["env"] = [
+            "TZ=\"America/Los_Angeles\""
+        ]
         self.optionalKWArgs["entrypoint"] = None
         self.optionalKWArgs["cmd"] = None
         self.optionalKWArgs["also"] = []
 
-        #We use the rootPath, not the buildPath.
+        # We use the rootPath, not the buildPath.
         self.clearBuildPath = False
 
         self.supportedImageOperatingSystems = [
             "debian",
             "alpine"
         ]
-        
-        
+
     # Required Builder method. See that class for details.
     def Build(self):
-        os.chdir(self.rootPath) # docker must be built from root.
-        
+        os.chdir(self.rootPath)  # docker must be built from root.
+
         self.shouldLogin = False
         if (self.docker_username is not None and self.docker_password is not None):
             self.shouldLogin = True
 
-        if (self.name == "Docker"):
+        if (self.image_name is None):
             if (self.docker_username is not None):
-                self.name = f"{self.docker_username}/{self.projectName}"
+                self.image_name = f"{self.docker_username}/{self.projectName}"
             else:
-                self.name = self.projectName
-        
+                self.image_name = self.projectName
+
         if (len(self.install) and self.image_os is None):
             raise OtherBuildError(f'You must specify the "os" if you wish to install packages')
 
         if (self.image_os is not None and self.image_os not in self.supportedImageOperatingSystems):
-            raise OtherBuildError(f'"os" {self.image_os} is not supported at this time. Please use one of {self.supportedImageOperatingSystems}')
+            raise OtherBuildError(
+                f'"os" {self.image_os} is not supported at this time. Please use one of {self.supportedImageOperatingSystems}')
 
         if (self.shouldLogin):
             self.LoginToDockerhub()
 
         self.WriteDockerfile()
         self.BuildDockerImage()
-        
+
         if (self.shouldLogin):
             self.PushDockerImage()
-
 
     def LoginToDockerhub(self):
         self.RunCommand(f"docker login -u=\"{self.docker_username}\" -p=\"{self.docker_password}\"")
 
     def BuildDockerImage(self):
-        self.RunCommand(f"docker build -t {self.name} .")
+        self.RunCommand(f"docker build -t {self.image_name} .")
 
     def PushDockerImage(self):
-        self.RunCommand(f"docker push {self.name}")
+        self.RunCommand(f"docker push {self.image_name}")
 
     def CopyToImage(self, externalPath, imagePath):
-        #This nonsense is required because we need `cp incPath/* buildpath/` behavior instead of `cp incPath buildpath/`
-        #TODO: is there a better way?
+        # This nonsense is required because we need `cp incPath/* buildpath/` behavior instead of `cp incPath buildpath/`
+        # TODO: is there a better way?
         for thing in os.listdir(externalPath):
             thingPath = os.path.relpath(os.path.join(externalPath, thing))
             thingName = os.path.basename(thing)
@@ -78,9 +81,10 @@ class docker(Builder):
                 self.dockerfile.write(f"COPY {thingPath} {imagePath}\n")
             elif os.path.isdir(thingPath):
                 self.dockerfile.write(f"COPY {thingPath} {imagePath}{thingName}\n")
-    
+
     def PrepForInstallation(self):
         if (self.image_os == "debian"):
+            self.dockerfile.write("ENV DEBIAN_FRONTEND=\"noninteractive\"\n")
             self.dockerfile.write("RUN apt update\n")
         elif (self.image_os == "alpine"):
             self.dockerfile.write("RUN apk update\n")
@@ -95,7 +99,7 @@ class docker(Builder):
         if (self.image_os == "debian"):
             self.dockerfile.write("RUN rm -rf /var/lib/apt/lists/*\n")
         elif (self.image_os == "alpine"):
-            pass #no cleanup necessary for alpine at this time.
+            pass  # no cleanup necessary for alpine at this time.
 
     def WriteDockerfile(self):
         self.dockerfile = open("Dockerfile", "w")
@@ -105,7 +109,7 @@ class docker(Builder):
 
         if (self.incPath is not None):
             self.CopyToImage(self.incPath, "/usr/local/include/")
-        
+
         if (self.libPath is not None):
             self.CopyToImage(self.libPath, "/usr/local/lib/")
 
@@ -113,19 +117,21 @@ class docker(Builder):
             self.CopyToImage(self.srcPath, "/usr/local/bin/")
             self.dockerfile.write("RUN chmod +x /usr/local/bin/*\n")
 
+        for env in self.env:
+            self.dockerfile.write(f"ENV {env}\n")
+
         if (len(self.install)):
             self.PrepForInstallation()
             for pkg in self.install:
                 self.InstallPackage(pkg)
             self.CleanInstallation()
 
-        if (len(self.also)):
-            for add in self.also:
-                self.dockerfile.write(add);
+        for add in self.also:
+            self.dockerfile.write(f"{add}\n");
 
         if (self.entrypoint is not None):
             self.dockerfile.write(f"ENTRYPOINT [\"{self.entrypoint}\"]\n")
-        
+
         if (self.cmd is not None):
             self.dockerfile.write(f"CMD [\"{self.cmd}\"]\n")
 
